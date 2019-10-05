9f4ed44daf4dce8584d862b9dbb4c9a1300b0a60
JulesPomme <julian.moreira.fr@gmail.com>
2019-10-05 18:56:53 +0200

    coin count update in game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,9 +9,34 @@ public class GameManager : MonoBehaviour {
         return instance;
     }
 
+    private int redCoinCount;
+    private int blueCoinCount;
+    private int greenCoinCount;
+    private int yellowCoinCount;
+
     private void Awake() {
         if (instance == null) {
             instance = this;
+        }
+    }
+
+    public void UpdateCoinCount(Coin.CoinColor color) {
+
+        switch (color) {
+            case Coin.CoinColor.RED:
+                redCoinCount++;
+                break;
+            case Coin.CoinColor.BLUE:
+                blueCoinCount++;
+                break;
+            case Coin.CoinColor.YELLOW:
+                yellowCoinCount++;
+                break;
+            case Coin.CoinColor.GREEN:
+                greenCoinCount++;
+                break;
+            default:
+                break;
         }
     }
 }
