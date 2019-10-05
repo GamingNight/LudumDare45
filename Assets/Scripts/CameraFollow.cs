5a9bf778c534df8b2e85d9cf6790150354cffb18
Leonaaard <contact.leo.faure@gmail.com>
2019-10-06 00:15:12 +0200

    trucs léo

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
+    private GameObject player;
     public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
-    public Transform target;
     public Vector3 offset = new Vector3 (0f,2f,-10f);
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void FixedUpdate()
     {
         Vector3 velocity = Vector3.zero;
-        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, 0.1f);
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref velocity, 0.1f);
     }
 
 }
---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        Vector3 dir = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
