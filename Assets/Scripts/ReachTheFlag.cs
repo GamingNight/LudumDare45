4783ada03fd938a293f9f52f171c5165547fce88
JulesPomme <julian.moreira.fr@gmail.com>
2019-10-05 22:52:13 +0200

    Prefab peaks

---
+++ Assets/Scripts/ReachTheFlag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachTheFlag : MonoBehaviour
+{
+
+    public GameObject endUI;
+    public GameObject gameContainer;
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+        if(col.gameObject.tag == "Player") {
+            gameContainer.SetActive(false);
+            endUI.SetActive(true);
+        }
+    }
+}
