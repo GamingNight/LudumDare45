5e7c5f7f81e28dd07d12420130e4f53997ab07dd
Leonaaard <contact.leo.faure@gmail.com>
2019-10-06 20:12:28 +0200

    switchs

---
+++ Assets/Scripts/Revealer/RevealerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealerAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+
+        animator = gameObject.GetComponent<Animator>();
+        StartCoroutine(waiter());
+
+    }
+
+    IEnumerator waiter()
+    {
+        while (true)
+        {
+            int waitTime = Random.Range(10, 25);
+            yield return new WaitForSeconds(waitTime);
+            animator.SetTrigger("Look");
+            yield return null;
+        }
+    }
+}
