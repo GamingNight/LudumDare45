dee7cd7898263870f20be7f6588d1522af5d4c51
Leonaaard <contact.leo.faure@gmail.com>
2019-10-06 12:33:46 +0200

    player blink

---
+++ Assets/Scripts/PlayerBlink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlink : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        
+        animator = gameObject.GetComponent<Animator>();
+        StartCoroutine(waiter());
+
+    }
+
+    IEnumerator waiter()
+    {
+        while (true)
+        {
+            int waitTime = Random.Range(5, 10);
+            yield return new WaitForSeconds(waitTime);
+            animator.SetTrigger("Blink");
+            yield return null;
+        }
+    }
+    
+}
