a6b15908bf81b7b159c940f7a0e013b82a572879
JulesPomme <julian.moreira.fr@gmail.com>
2019-10-07 13:48:01 +0200

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,12 +6,12 @@ public class CameraFollow : MonoBehaviour {
     public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
     public Vector3 offset = new Vector3(0f, 2f, -10f);
-    public Vector4 leftRightDownUpBoundaries;
+    //public Vector4 leftRightDownUpBoundaries;
 
     void FixedUpdate() {
         Vector3 velocity = Vector3.zero;
         Vector3 targetPosition = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref velocity, dampTime);
-        targetPosition = new Vector3(Mathf.Max(leftRightDownUpBoundaries.x, Mathf.Min(leftRightDownUpBoundaries.y, targetPosition.x)), Mathf.Max(leftRightDownUpBoundaries.z, Mathf.Min(leftRightDownUpBoundaries.w, targetPosition.y)), targetPosition.z);
+        //targetPosition = new Vector3(Mathf.Max(leftRightDownUpBoundaries.x, Mathf.Min(leftRightDownUpBoundaries.y, targetPosition.x)), Mathf.Max(leftRightDownUpBoundaries.z, Mathf.Min(leftRightDownUpBoundaries.w, targetPosition.y)), targetPosition.z);
         transform.position = targetPosition;
     }
 
